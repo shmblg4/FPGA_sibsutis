4584274cd17c0c24eb10a97c647429987188fa90
Matvey Gotfrid <matveygot@yandex.ru>
2024-12-15 16:59:41 +0700

    Fixed bugs

--- com-manager/src/main.cpp
+++ com-manager/src/main.cpp
@@ -6,10 +6,12 @@
 #include <condition_variable>
 #include <queue>
 #include <xserial.hpp>
+#include <windows.h>
+#include <mmsystem.h>
+#include <mmreg.h>
 
 #define SAMPLE_RATE 24000
 #define BUFFER_SIZE 8192
-#define NUM_BUFFERS 2
 #define DELAY_SECONDS 2
 
 std::atomic<bool> stopFlag(false);
@@ -39,7 +41,7 @@ public:
         if (count < capacity)
             ++count;
         else
-            readIndex = (readIndex + 1) % capacity; // overwriting old data
+            readIndex = (readIndex + 1) % capacity; // Overwrite old data
         cv.notify_one();
     }
 
@@ -58,7 +60,7 @@ public:
     }
 };
 
-// Using xserial to work with COM ports
+// Function to open COM port using xserial
 xserial::ComPort openCOMPort(int portNumber, unsigned long baudRate)
 {
     xserial::ComPort comPort;
@@ -74,14 +76,13 @@ xserial::ComPort openCOMPort(int portNumber, unsigned long baudRate)
 void playbackAudio(CircularBuffer &buffer)
 {
     HWAVEOUT hWaveOut;
-    WAVEFORMATEX waveFormat;
+    WAVEFORMATEX waveFormat = {};
     waveFormat.wFormatTag = WAVE_FORMAT_PCM;
-    waveFormat.nChannels = 1; // mono
+    waveFormat.nChannels = 1;
     waveFormat.nSamplesPerSec = SAMPLE_RATE;
     waveFormat.wBitsPerSample = 16;
     waveFormat.nBlockAlign = waveFormat.nChannels * waveFormat.wBitsPerSample / 8;
     waveFormat.nAvgBytesPerSec = waveFormat.nSamplesPerSec * waveFormat.nBlockAlign;
-    waveFormat.cbSize = 0;
 
     if (waveOutOpen(&hWaveOut, WAVE_MAPPER, &waveFormat, 0, 0, CALLBACK_NULL) != MMSYSERR_NOERROR)
     {
@@ -89,25 +90,25 @@ void playbackAudio(CircularBuffer &buffer)
         return;
     }
 
-    WAVEHDR waveHeader;
     std::vector<short> audioData(BUFFER_SIZE);
 
     while (!stopFlag)
     {
         if (buffer.pop(audioData))
         {
+            WAVEHDR waveHeader = {};
             waveHeader.lpData = reinterpret_cast<LPSTR>(audioData.data());
             waveHeader.dwBufferLength = audioData.size() * sizeof(short);
-            waveHeader.dwFlags = 0;
 
-            if (waveOutPrepareHeader(hWaveOut, &waveHeader, sizeof(WAVEHDR)) != MMSYSERR_NOERROR)
+            if (waveOutPrepareHeader(hWaveOut, &waveHeader, sizeof(WAVEHDR)) == MMSYSERR_NOERROR)
             {
-                std::cerr << "Failed to prepare header for output." << std::endl;
-                waveOutClose(hWaveOut);
-                return;
+                waveOutWrite(hWaveOut, &waveHeader, sizeof(WAVEHDR));
+                while (!(waveHeader.dwFlags & WHDR_DONE))
+                {
+                    Sleep(10); // Wait until playback is complete
+                }
+                waveOutUnprepareHeader(hWaveOut, &waveHeader, sizeof(WAVEHDR));
             }
-
-            waveOutWrite(hWaveOut, &waveHeader, sizeof(WAVEHDR));
         }
     }
 
@@ -132,14 +133,13 @@ void receiveFromCOMPort(xserial::ComPort &comPort, CircularBuffer &circBuffer)
 void recordAudio(CircularBuffer &buffer)
 {
     HWAVEIN hWaveIn;
-    WAVEFORMATEX waveFormat;
+    WAVEFORMATEX waveFormat = {};
     waveFormat.wFormatTag = WAVE_FORMAT_PCM;
-    waveFormat.nChannels = 1; // mono
+    waveFormat.nChannels = 1;
     waveFormat.nSamplesPerSec = SAMPLE_RATE;
     waveFormat.wBitsPerSample = 16;
     waveFormat.nBlockAlign = waveFormat.nChannels * waveFormat.wBitsPerSample / 8;
     waveFormat.nAvgBytesPerSec = waveFormat.nSamplesPerSec * waveFormat.nBlockAlign;
-    waveFormat.cbSize = 0;
 
     if (waveInOpen(&hWaveIn, WAVE_MAPPER, &waveFormat, 0, 0, CALLBACK_NULL) != MMSYSERR_NOERROR)
     {
@@ -147,11 +147,10 @@ void recordAudio(CircularBuffer &buffer)
         return;
     }
 
-    WAVEHDR waveHeader;
+    WAVEHDR waveHeader = {};
     std::vector<short> audioData(BUFFER_SIZE);
     waveHeader.lpData = reinterpret_cast<LPSTR>(audioData.data());
     waveHeader.dwBufferLength = BUFFER_SIZE * sizeof(short);
-    waveHeader.dwFlags = 0;
 
     if (waveInPrepareHeader(hWaveIn, &waveHeader, sizeof(WAVEHDR)) != MMSYSERR_NOERROR)
     {
@@ -167,6 +166,10 @@ void recordAudio(CircularBuffer &buffer)
         if (waveInAddBuffer(hWaveIn, &waveHeader, sizeof(WAVEHDR)) == MMSYSERR_NOERROR)
         {
             buffer.push(audioData);
+            while (!(waveHeader.dwFlags & WHDR_DONE))
+            {
+                Sleep(10);
+            }
         }
     }
 
@@ -189,22 +192,22 @@ void sendToCOMPort(xserial::ComPort &comPort, CircularBuffer &circBuffer)
 
 int main()
 {
-    int Port;
-    unsigned long baudRate = 921600; // Setting the required speed
-    std::cout << "Select COM port (e.g., 1 for COM1): ";
-    std::cin >> Port;
-
-    // Open the COM port via xserial
-    xserial::ComPort comWrite = openCOMPort(Port, baudRate);
-    if (!comWrite.getStateComPort())
+    int port;
+    unsigned long baudRate = 921600;
+    xserial::ComPort ListOfPorts;
+    std::vector<std::string> ComPorts;
+    ListOfPorts.getListSerialPorts(std::ref(ComPorts));
+    for (size_t i = 0; i < ComPorts.size(); i++)
     {
-        return 1;
+        std::cout << ComPorts[i] << std::endl;
     }
+    std::cout << "Select COM port (e.g., 1 for COM1): ";
+    std::cin >> port;
+    ListOfPorts.close();
 
-    xserial::ComPort comRead = openCOMPort(Port, baudRate);
-    if (!comRead.getStateComPort())
+    xserial::ComPort comPort = openCOMPort(port, baudRate);
+    if (!comPort.getStateComPort())
     {
-        comWrite.close();
         return 1;
     }
 
@@ -212,8 +215,8 @@ int main()
     CircularBuffer playbackBuffer(DELAY_SECONDS * SAMPLE_RATE / BUFFER_SIZE);
 
     std::thread recordThread(recordAudio, std::ref(recordBuffer));
-    std::thread sendThread(sendToCOMPort, std::ref(comWrite), std::ref(recordBuffer));
-    std::thread receiveThread(receiveFromCOMPort, std::ref(comRead), std::ref(playbackBuffer));
+    std::thread sendThread(sendToCOMPort, std::ref(comPort), std::ref(recordBuffer));
+    std::thread receiveThread(receiveFromCOMPort, std::ref(comPort), std::ref(playbackBuffer));
     std::thread playbackThread(playbackAudio, std::ref(playbackBuffer));
 
     std::cin.get();
@@ -226,8 +229,7 @@ int main()
     receiveThread.join();
     playbackThread.join();
 
-    comWrite.close();
-    comRead.close();
+    comPort.close();
 
     return 0;
 }
